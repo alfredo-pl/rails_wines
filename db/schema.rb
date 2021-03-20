7d3c1b780dd966ab9dd79f248938af853a757847
Alfredo Peña <aalfredoplz@gmail.com>
2021-03-20 12:07:56 -0300

    migration role to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_134521) do
+ActiveRecord::Schema.define(version: 2021_03_20_135343) do
 
   create_table "assemblies", force: :cascade do |t|
     t.integer "wine_id", null: false
@@ -42,8 +42,10 @@ ActiveRecord::Schema.define(version: 2021_03_20_134521) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
   create_table "wines", force: :cascade do |t|
@@ -54,4 +56,5 @@ ActiveRecord::Schema.define(version: 2021_03_20_134521) do
 
   add_foreign_key "assemblies", "strains"
   add_foreign_key "assemblies", "wines"
+  add_foreign_key "users", "roles"
 end
