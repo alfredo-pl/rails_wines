754d366907b609588e4d3be3a36f48b95b046d09
Alfredo Peña <aalfredoplz@gmail.com>
2021-03-20 12:06:15 -0300

    method autorize admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def authorize_admin!
+        unless current_user.role_id == 1
+            flash[:alert]= "You must be an Admin to access this section"
+            redirect_to root_path
+            
+        end
+    end
 end
