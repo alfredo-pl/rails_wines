06dce4cf21997680660c60d10edb6a50e55a2206
Alfredo Peña <aalfredoplz@gmail.com>
2021-03-20 12:06:43 -0300

    root path wines

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :assemblies
   resources :wines
   resources :strains
-  root 'strains#index'
+  root 'wines#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
