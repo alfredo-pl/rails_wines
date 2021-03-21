9dc6da0a222fbe367ec2b037409706f7c349b441
Alfredo Peña <aalfredoplz@gmail.com>
2021-03-21 11:42:09 -0300

    params score

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -64,6 +64,6 @@ class WinesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def wine_params
-      params.require(:wine).permit(:name, strain_ids:[])
+      params.require(:wine).permit(:name, :score, strain_ids:[])
     end
 end
