75d7cba1b1402e286db37d1df635f873adcc7132
Alfredo Peña <aalfredoplz@gmail.com>
2021-03-22 16:17:15 -0300

    method nameStrain

--- app/models/assembly.rb
+++ app/models/assembly.rb
@@ -1,4 +1,10 @@
 class Assembly < ApplicationRecord
   belongs_to :wine
   belongs_to :strain
+  accepts_nested_attributes_for :strain, allow_destroy: true
+
+  def nameStrain
+    s =  Strain.where(id: strain_id).pluck :name
+    s.first
+  end
 end
