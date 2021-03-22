054ae7627ded24d46e73192af3da64839534c7e8
Alfredo Peña <aalfredoplz@gmail.com>
2021-03-22 16:17:40 -0300

    method build strains

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,5 +1,16 @@
 class Wine < ApplicationRecord
-    has_many :assemblies
-    has_many :strains, through: :assemblies
-    validates :strains,  presence: true
+    has_many :assemblies, dependent: :destroy
+    has_many :strains, through: :assemblies, dependent: :destroy
+    accepts_nested_attributes_for :assemblies, allow_destroy: true
+
+    validates_associated :assemblies
+    validates_associated :strains
+
+    def build_strains
+        a = Assembly.references(:wines).where(wine_id: id)
+        
+        a.map{|s| "#{s.nameStrain} (#{s.proportion}%)" }.join(", ")
+    end
+
+    
 end
