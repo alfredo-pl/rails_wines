6370ebcf06bbf4d2ec9fad407a44aa7a0fcc1950
Alfredo Peña <aalfredoplz@gmail.com>
2021-03-26 09:49:53 -0300

    validate  name

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -1,7 +1,5 @@
 class Strain < ApplicationRecord
     has_many :assemblies
     has_many :wines, through: :assemblies
-
-    validates :name, uniqueness: true
-    
+    validates :name, uniqueness: true ,presence: true
 end
