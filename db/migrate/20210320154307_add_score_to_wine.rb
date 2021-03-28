7b1ff395d9ae14bf7605a76e5033cce8d2673016
Alfredo Peña <aalfredoplz@gmail.com>
2021-03-28 01:17:05 -0300

    int to numeric

--- db/migrate/20210320154307_add_score_to_wine.rb
+++ db/migrate/20210320154307_add_score_to_wine.rb
@@ -1,5 +1,5 @@
 class AddScoreToWine < ActiveRecord::Migration[6.1]
   def change
-    add_column :wines, :score, :integer, default: 0, limit: 100
+    add_column :wines, :score, :numeric, default: 0, limit: 100
   end
 end
