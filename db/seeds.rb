868cd944b0bf303c385202919aaab711b7b027de
Alfredo Peña <aalfredoplz@gmail.com>
2021-03-28 01:17:15 -0300

    data in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Role.create(name: "Admin")
+Role.create(name: "Visit")
+Role.create(name: "Oenologist")
+
+User.create(email: "Admin@example.com" , password: "123123" , password_confirmation: "123123", role_id: 1 )
+User.create(email: "Visit@example.com" , password: "123123" , password_confirmation: "123123", role_id: 2 )
+User.create(email: "Oeno@example.com" , password: "123123" , password_confirmation: "123123", role_id: 3 )
+
+strains = [ "Reisling","Pino Gris","Sauvignon Blanc","Chardonnay","Pinot Noir","Zinfandel","Syrah","Cabernet Sauvignon"]
+strains.each { |strain| Strain.create(name: strain) }
