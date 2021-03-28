0b191f7e1f2eecb8e734ffdb5ea9fc708c509636
Alfredo Peña <aalfredoplz@gmail.com>
2021-03-28 00:40:13 -0300

    wines controller spec

--- spec/controllers/wines_controller_spec.rb
+++ spec/controllers/wines_controller_spec.rb
@@ -1,25 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe WinesController, :type => :controller do
-    describe 'GET wines#index' do
-      it "assigns @wines" do 
-        role = Role.create(id: 1, name:"visit")
-        @user = User.create(id: 1 , name: "test@example.com", password: "123123", password_confirmation: "123123", role_id: 1)
-        sign_in @user
-        get :index
-        expect(response).to render_template("index")
-    end
-  end
-
-    describe 'You should log in to see the index' do
-        it 'should redirect to sign_in view' do
+RSpec.describe WinesController, :type​ => :controller​​ do
+    describe 'GET index' do
+        it 'returns a successful response' do
+            role = Role.create(id: 1, name: "Admin")
+            @user = User.create(email: 'test@gmail.com', password: '12345678', password_confirmation:'12345678' ,role_id: 1)
+            sign_in @user
             get :index
-            expect(response).to_not render_template("index")
+            expect(response).to be_successful
+        end
+     
+        it 'assigns wines' do
+            @user = User.create(email: 'test@gmail.com', password: '12345678')
+            sign_in @user
+            @wine = Wine.create(name: 'Gran vino diaulo', score: 58, age: 20)
+            get :index
+            expect(Wine.count).to eq(1)
+        end
+    
+        it 'renders the index template' do
+            role = Role.create(id: 1, name: "Admin")
+            @user = User.create(email: 'test@gmail.com', password: '12345678', password_confirmation:'12345678' ,role_id: 1)
+            sign_in @user
+            get :index
+            expect(response).to render_template('index')
         end
 
-        it 'should redirect to sign_in view' do
-            get :index
-            expect(response).to redirect_to new_user_session_path
+        it 'renders the show template' do
+            role = Role.create(id: 1, name: "Admin")
+            @user = User.create(email: 'test@gmail.com', password: '12345678', password_confirmation:'12345678' ,role_id: 1)
+            sign_in @user
+            wine = Wine.create(name: 'Gran vino diaulo', score: '58' ,age: 20 )
+            get :show, params: {id:wine.id} 
+            expect(response).to render_template('show')
         end
     end
 end
